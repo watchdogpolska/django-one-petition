94783a63c2f9757848d755616527db9bee40476b
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-30 02:37:03 +0200

    Fix url for thank_you page

--- petition/urls.py
+++ petition/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^(?P<slug>[\w-]+)/new/$', views.SignatureList.as_view(), name='list'),
     url(r'^api/$', views.SignatureApiList.as_view(), name='list_api'),
     url(r'^(?P<slug>[\w-]+)/api/$', views.SignatureApiList.as_view(), name='list_api'),
-    url(r'^/thank-you/$', views.SignatureCreateDone.as_view(), name="thank-you"),
+    url(r'^thank-you/$', views.SignatureCreateDone.as_view(), name="thank-you"),
     url(r'^(?P<slug>[\w-]+)/text/$', views.PetitionDetail.as_view(), name="details"),
 
 )
