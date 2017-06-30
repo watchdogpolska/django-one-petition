004bbfdfe5e80073498ed4adf043ab0b3ba277f6
Adam Dobrawy <ad-m@users.noreply.github.com>
2017-06-30 04:15:57 +0200

    Update 0008_auto_20150728_0244.py

--- petition/migrations/0008_auto_20150728_0244.py
+++ petition/migrations/0008_auto_20150728_0244.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='signature',
-            options={},
+            options={'swappable': swapper.swappable_setting('petition', 'Petition')},
         ),
     ]
